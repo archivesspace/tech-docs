ded130fe96887b2f1ba3cd8c1d47173af076c6e8
Brian Zelip <brian.zelip@lyrasis.org>
2024-09-12 16:18:25 -0400

    Customize home page, logo, and color styles

--- ASTRO/astro.config.mjs
+++ ASTRO/astro.config.mjs
@@ -3,25 +3,30 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://docs.archivesspace.org',
+  integrations: [
+    starlight({
+      title: 'ArchivesSpace Tech Docs',
+      logo: {
+        light: './src/assets/logo.svg',
+        dark: './src/assets/logo-dark.svg',
+        replacesTitle: true
+      },
+      customCss: ['./src/styles/custom.css'],
+      editLink: {
+        baseUrl: 'https://github.com/archivesspace/tech-docs/edit/master/'
+      },
+      lastUpdated: true,
+      social: {
+        github: 'https://github.com/archivesspace/tech-docs',
+        youtube: 'https://www.youtube.com/@archivesspace5340'
+      },
+      sidebar: [
+        {
+          label: 'Tech Docs',
+          link: '/'
+        }
+      ]
+    })
+  ]
 });
