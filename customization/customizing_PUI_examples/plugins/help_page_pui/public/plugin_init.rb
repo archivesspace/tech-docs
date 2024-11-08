b5871a9d512b65b1f59e1071296bec98805be025
Brian Zelip <brian.zelip@lyrasis.org>
2024-11-08 14:33:21 -0500

    Remove outdated content and Jekyll artifacts

--- customization/customizing_PUI_examples/plugins/help_page_pui/public/plugin_init.rb
+++
@@ -1,2 +0,0 @@
-Plugins::extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
-Plugins::add_menu_item('help', 'Help')
--- customization/customizing_PUI_examples/plugins/help_page_pui/public/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  get '/help', to: 'help#index'
-end
