b6bde21a42afab7619c9726754bf8859cc6fa6cb
Brian Zelip <brian.zelip@lyrasis.org>
2024-11-10 08:12:09 -0500

    Add and update Cypress, add tests, update CI,
    Update node version info
    Remove unnecessary files

---
+++ cypress/e2e/sitemap.cy.js
@@ -0,0 +1,22 @@
+describe("Sitemap", () => {
+  it("assets are present", () => {
+    cy.request("/sitemap-index.xml");
+    cy.request("/sitemap-0.xml");
+  });
+
+  it("is linked from each page", () => {
+    cy.visit("/");
+    cy.get("link[rel='sitemap']").should(
+      "have.attr",
+      "href",
+      "/sitemap-index.xml",
+    );
+
+    cy.visit("/architecture/jsonmodel");
+    cy.get("link[rel='sitemap']").should(
+      "have.attr",
+      "href",
+      "/sitemap-index.xml",
+    );
+  });
+});
