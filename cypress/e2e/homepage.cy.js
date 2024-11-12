4aadd2344df71e22b6391193890a31161f94f664
Brian Zelip <brian.zelip@lyrasis.org>
2024-11-12 10:18:23 -0500

    Format cypress and config files, update about dev docs

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,16 +1,16 @@
-describe("Home page", () => {
-  it("shows the sidebar, custom HomePage component, and Starlight credits", () => {
-    cy.visit("/");
-    cy.get("#starlight__sidebar");
-    cy.get("[data-is-homepage]");
-    cy.get("footer").contains("Built with Starlight");
-  });
+describe('Home page', () => {
+  it('shows the sidebar, custom HomePage component, and Starlight credits', () => {
+    cy.visit('/')
+    cy.get('#starlight__sidebar')
+    cy.get('[data-is-homepage]')
+    cy.get('footer').contains('Built with Starlight')
+  })
 
-  it("does not show a Table of Contents, edit page link, report issue link, and last updated date", () => {
-    cy.visit("/");
-    cy.get(".right-sidebar").should("not.exist");
-    cy.get("footer").contains("Edit this page on GitHub").should("not.exist");
-    cy.get("footer").contains("Report an issue on Jira").should("not.exist");
-    cy.get("footer").contains("Last updated: ").should("not.exist");
-  });
-});
+  it('does not show a Table of Contents, edit page link, report issue link, and last updated date', () => {
+    cy.visit('/')
+    cy.get('.right-sidebar').should('not.exist')
+    cy.get('footer').contains('Edit this page on GitHub').should('not.exist')
+    cy.get('footer').contains('Report an issue on Jira').should('not.exist')
+    cy.get('footer').contains('Last updated: ').should('not.exist')
+  })
+})
