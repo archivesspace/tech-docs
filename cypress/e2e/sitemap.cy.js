7e7aa92c23d3e65f517902d1e6d38ab94929beef
Brian Zelip <brian.zelip@lyrasis.org>
2025-01-21 06:23:25 -0500

    Tweak content pages and sitemap tests

--- cypress/e2e/sitemap.cy.js
+++ cypress/e2e/sitemap.cy.js
@@ -1,24 +1,18 @@
+const indexPath = '/sitemap-index.xml'
+
 describe('Sitemap', () => {
   if (Cypress.env('MODE') === 'production') {
     it('assets are present', () => {
-      cy.request('/sitemap-index.xml')
+      cy.request(indexPath)
       cy.request('/sitemap-0.xml')
     })
   }
 
   it('is linked from each page', () => {
     cy.visit('/')
-    cy.get("link[rel='sitemap']").should(
-      'have.attr',
-      'href',
-      '/sitemap-index.xml'
-    )
+    cy.get("link[rel='sitemap']").should('have.attr', 'href', indexPath)
 
     cy.visit('/architecture/jsonmodel')
-    cy.get("link[rel='sitemap']").should(
-      'have.attr',
-      'href',
-      '/sitemap-index.xml'
-    )
+    cy.get("link[rel='sitemap']").should('have.attr', 'href', indexPath)
   })
 })
