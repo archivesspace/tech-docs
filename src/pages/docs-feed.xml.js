dcb7cddc90104ebb45b57732c052040f950bd2d5
dakuojas <dakuojas@gmail.com>
2026-04-22 14:14:42 +0530

    add feed for blog and docs

---
+++ src/pages/docs-feed.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('docs');
+  return rss({
+    title: 'TechDocs',
+    description: 'An ArchiveNaut\'s guide to the ArchivesSpace',
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      // Compute RSS link from post `id`
+      // This example assumes all posts are rendered as `/[id]` routes
+      link: `/${post.id}/`,
+    })),
+  });
+}
