6f1d1200eb827e4ffbc71dce9f292ce6ec00d5d3
dakuojas <dakuojas@gmail.com>
2026-04-22 15:06:45 +0530

    prettier fix

--- src/pages/blog-feed.xml.js
+++ src/pages/blog-feed.xml.js
@@ -1,8 +1,8 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-  const blog = await getCollection('blog');
+  const blog = await getCollection('blog')
   return rss({
     title: 'Blog',
     description: 'Technical updates from ArchivesSpace developers',
@@ -13,7 +13,7 @@ export async function GET(context) {
       description: post.data.metaDescription,
       // Compute RSS link from post `id`
       // This example assumes all posts are rendered as `/blog/[id]` routes
-      link: `/blog/${post.id}/`,
-    })),
-  });
+      link: `/blog/${post.id}/`
+    }))
+  })
 }
